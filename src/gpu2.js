6f9942d2faa39fa310d511b529e14b12c8ddef21
Ian <typppo@gmail.com>
2014-01-30 21:03:59 -0800

    working on padding

--- src/gpu2.js
+++ src/gpu2.js
@@ -4,20 +4,23 @@
   var me = this;
   var resultOffset = 0;
 
+  var pow2 = [4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096];
+
   me.prepare = function(id) {
     return me.prepareString(document.getElementById(id).innerHTML);
   };
 
-  me.setResultOffset = function(val) {
-    resultOffset = val;
+  me.setResultRange = function(min, max) {
+    resultOffset = Math.max(Math.abs(min), Math.abs(max));
   };
 
   me.prepareString  = function(glsl_str) {
     var length = -1;
+    var pad = 0;
     var webCLGL = new WebCLGL();
 
     return function() {
-      var args = arguments;
+      var args = Array.prototype.slice.call(arguments);
       var buffers = [];
 
       for (var i=0; i < args.length; i++) {
@@ -36,11 +39,37 @@
         }
 
         if (length < 0) {
-          // Set length requirement
+          // Length requirement not set yet
           length = arg.length;
+          if (!isPow2(length) || length < 4) {
+            // Calculate amount of padding
+            // need to pad if it's not a power of 2!  Or less than 4
+            var nextPow2 = -1;
+            for (var j=0; j < pow2.length; j++) {
+              var pp = pow2[j];
+              if (pp >= length) {
+                pad = pp - length;
+                break;
+              }
+            }
+            if (pad < 0) {
+              // Failed, maybe because this is a really long list
+              // User is responsible for enforcing power of 2 now
+              console.error('Array arguments must be a power of 2 in length');
+              return false;
+            }
+          }
         } else if (arg.length !== length) {
-          console.error('Required length', length);
+          console.error('Required length', length, 'but got', arg.length + pad);
+          console.log('For arg:', arg);
           return false;
+        }
+
+        if (pad > 0) {
+          // Pad to power of 2 length
+          for (var j=0; j < pad; j++) {
+            arg.push(0);
+          }
         }
 
         var offset = 0;
@@ -89,12 +118,16 @@
 
       // TODO get result, either an array of floats or an array of float4s
       // TODO Final offset can't really be predicted
-      var buffer_ret = webCLGL.createBuffer(length, 'FLOAT', resultOffset);
+      var buffer_ret = webCLGL.createBuffer(length+pad, 'FLOAT', resultOffset);
       webCLGL.enqueueNDRangeKernel(kernel_add, buffer_ret);
 
       var result = webCLGL.enqueueReadBuffer_Float(buffer_ret);
       return result;
     };
   };
+
+  function isPow2(n) {
+    return n != 0 && (n & (n - 1) == 0)
+  }
 
 };
